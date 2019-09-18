b98df4a8530e37a2aa4ddff02003087738c97dc4
Marcus Vinicius Patriota <macusvp@gmail.com>
2019-09-17 22:56:29 -0300

    Update UsuarioServiceTest.java

--- src/test/java/sefaz/pe/gov/br/desafiosefaz/services/UsuarioServiceTest.java
+++ src/test/java/sefaz/pe/gov/br/desafiosefaz/services/UsuarioServiceTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sefaz.pe.gov.br.desafiosefaz.services;
 
 import java.util.List;
@@ -16,7 +11,7 @@ import sefaz.pe.gov.br.desafiosefaz.model.Usuario;
 
 /**
  *
- * @author marcu
+ * @author Marcus Patriota
  */
 public class UsuarioServiceTest {
     
